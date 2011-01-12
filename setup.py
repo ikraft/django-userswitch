ccd865f590decdc451fb779dd2515a74e3aff3b8
Mir Nazim <github@mirnazim.org>
2011-01-12 14:00:58 +0530

    Updates to setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version = "0.1a",
     packages = ['userswitch',],
     license = "New BSD License",
-    author = open('AUTHORS.rst').read(),
+    author = "iKraft Software (P) LTD.",
     author_email = "hello@ikraftsoft.com",
-    description = open('README.rst').read(),
+    description = "A quick 'n' dirty django app for switching between different users while testing",
     url = "https://github.com/ikraftsoft/django-userswitch",
     include_package_data=True,
 )
