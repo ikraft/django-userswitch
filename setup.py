17a2d154a232f4bb4f23de48977e1035afe6febc
Owais <loneowais@gmail.com>
2011-01-12 14:49:16 +0530

    Added some more information to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,14 @@ setup(
     license = "New BSD License",
     author = "iKraft Software (P) LTD.",
     author_email = "hello@ikraftsoft.com",
+    maintainer = "iKraft Software (P) LTD.",
+    maintainer_email = "hello@ikraftsoft.com",
     description = "A quick 'n' dirty Django app for switching between different users while testing",
+    long_description=open('README.rst').read(),
     url = "https://github.com/ikraftsoft/django-userswitch",
+    download_url="https://github.com/ikraftsoft/django-userswitch/tarball/master",
+    install_requires=[
+        'Django>=1.2',
+    ],
     include_package_data=True,
 )
